828eda4f6e9081f31f5675ace1e366a88bcc4100
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-17 14:32:28 +0300

    Challenge2_Done step3

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -39,3 +39,14 @@ console.log("canDrive:", canDrive);
 console.log("canRentCar:", canRentCar);
 console.log("needDriver:", needDriver);
 
+const role = "admin";
+switch (role) {
+    case "admin":
+        console.log("You have read and write access for the data"); break;
+    case "user":
+        console.log("You have only read access for the data"); break;
+    case "guest":
+        console.log("You have no access for the data"); break;
+    default:
+        console.log("User role is not recoginized");
+}
