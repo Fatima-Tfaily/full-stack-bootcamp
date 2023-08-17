82b3cee6d9c43ecea40d4580e945b491fce303fb
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-17 13:08:38 +0300

    Challege1_Done step3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,3 +1,42 @@
 console.log('I am linked now!');
 var name = 'Fatima Tfaily';
 alert(name);
+var ageVar = 18;
+var lastNameVar = "Tfaily";
+var DOBVar = "19-5-2005";
+
+
+let ageLet = 20;
+let lastNameLet = "Johnson";
+let DOBLet = "9-01-2003";
+
+const ageConst = 33;
+const lastNameConst = "Williams";
+const DOBConst = "10-03-1990";
+
+ageVar = 26;
+lastNameVar = "Dived";
+DOBVar = "22-06-1992";
+
+ageLet = 40;
+lastNameLet = "Michayel";
+DOBLet = "5-12-1983";
+//error because is a constant 
+//ageConst = 41;
+//lastNameConst = "Davis";
+//DOBConst = "15-01-1982";
+
+console.log("Using var:");
+console.log("Age:", ageVar);
+console.log("Last Name:", lastNameVar);
+console.log("Date of Birth:", DOBVar);
+
+console.log("Using let:");
+console.log("Age:", ageLet);
+console.log("Last Name:", lastNameLet);
+console.log("Date of Birth:", DOBLet);
+
+console.log("Using const:");
+console.log("Age:", ageConst);
+console.log("Last Name:", lastNameConst);
+console.log("Date of Birth:", DOBConst);
