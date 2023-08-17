74b4936116bce489f410942bcb1cde81fb6ce813
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-17 14:12:38 +0300

    Challenge2_Done step1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,17 @@
+let age = 18;
+if (age >= 20) {
+    console.log("You can enter the room")
+}
+else {
+    console.log("You can't enter the room")
+}
+
+const expression1 = 5 > 3;
+const expression2 = 10 === '10';
+const expression3 = true && false;
+const expression4 = !expression3;
+
+console.log("expression1:", expression1);
+console.log("expression2:", expression2);
+console.log("expression3:", expression3);
+console.log("expression4:", expression4);
