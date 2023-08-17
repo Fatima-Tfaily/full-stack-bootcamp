87ecec3bc0e015d761481886088fd0153c0e7ae5
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-17 14:23:53 +0300

    Challenge_Done step2

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -1,5 +1,5 @@
-let age = 18;
-if (age >= 20) {
+let age1 = 18;
+if (age1 >= 20) {
     console.log("You can enter the room")
 }
 else {
@@ -15,3 +15,27 @@ console.log("expression1:", expression1);
 console.log("expression2:", expression2);
 console.log("expression3:", expression3);
 console.log("expression4:", expression4);
+
+const age2 = 25;
+const hasDriverLicense = true;
+const hasCar = false;
+
+let canDrive = false;
+if (age2 >= 18 && hasDriverLicense) {
+    canDrive = true;
+}
+
+let canRentCar = false;
+if (age2 >= 22 && hasDriverLicense && !hasCar) {
+    canRentCar = true;
+}
+
+let needDriver = false;
+if (age2 < 18 && !hasDriverLicense && !hasCar) {
+    needDriver = true;
+}
+
+console.log("canDrive:", canDrive);
+console.log("canRentCar:", canRentCar);
+console.log("needDriver:", needDriver);
+
