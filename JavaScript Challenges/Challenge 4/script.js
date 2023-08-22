c85527f57d2dd968b0b373978c666e15e91c5179
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-22 14:31:02 +0300

    Challenge4_Done step1

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,12 @@
+let fiveNumbers = [];
+fiveNumbers = [1, 2, 3, 4, 5];
+
+let personalInfo = {
+    name: 'Fatima',
+    age: 18,
+    height: 158,
+    location: 'Baalbeck,chmestar',
+    nationality: 'libanaise'
+}
+
+
