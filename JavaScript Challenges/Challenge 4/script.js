2fbc7fff56e3bebddd34310e30606cd808fc531d
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-22 14:53:22 +0300

    Challenge4_Done with step2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -9,4 +9,26 @@ let personalInfo = {
     nationality: 'libanaise'
 }
 
+let nextThreeNumbers = new Array();
+nextThreeNumbers = [6, 7, 8];
+let allNumbers = [];
+for (let i = 0; i < fiveNumbers.length; i++) {
+    allNumbers.push(fiveNumbers[i]);
+}
+for (let i = 0; i < nextThreeNumbers.length; i++) {
+    allNumbers.push(nextThreeNumbers[i]);
+}
+console.log(allNumbers);
+let additionalInfo = {};
+additionalInfo = {
+    occupation: 'none',
+    hobby: 'reading',
+    education: 'first year in computerScience'
+}
+let fullInfo = {};
+fullInfo = {
+    ...personalInfo,
+    ...additionalInfo
+}
+console.log(fullInfo);
 
