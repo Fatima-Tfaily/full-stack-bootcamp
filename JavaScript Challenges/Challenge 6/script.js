35fe7f7a52b57b7fe225f4f7244565b4b4906bd1
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-24 16:58:45 +0300

    Challenge6_ Done with step2

--- JavaScript Challenges/Challenge 6/script.js
+++ JavaScript Challenges/Challenge 6/script.js
@@ -31,6 +31,40 @@ delayedPromise()
     .then((chainedMessage) => {
         console.log(chainedMessage);
     });
+function simulateNetworkRequest() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() < 0.5) {
+                resolve("Data fetched successfully!");
+            } else {
+                reject("Network Error!");
+            }
+        }, 3000); // 3000 milliseconds = 3 seconds
+    });
+}
+simulateNetworkRequest()
+    .then((message) => {
+        console.log(message); // Success scenario
+    })
+    .catch((error) => {
+        console.error(error); // Error scenario
+    });
+async function fetchPost() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+}
+
+fetchPost();
 
 
 
