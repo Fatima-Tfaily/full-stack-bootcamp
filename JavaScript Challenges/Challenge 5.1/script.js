406da2c604f7c602b98bd92a3bae12395a1c443d
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-24 15:34:42 +0300

    Challenge 5.1_Done with step2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -13,3 +13,28 @@ button.addEventListener('click', () => {
     let randomNumber = Math.floor(Math.random() * 1000) + 1;
     button.textContent = "Clicked " + randomNumber + "!";
 })
+const h1 = document.createElement("h1");
+h1.textContent = "Hello!";
+document.body.appendChild(h1);
+h1.style.border = "10px dotted green";
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "a":
+            h1.style.backgroundColor = "red";
+            break;
+        case "s":
+            h1.style.marginLeft = (parseInt(h1.style.marginLeft) || 0) + 10 + "px";
+            break;
+        case "d":
+            const newParagraph = document.createElement("p");
+            newParagraph.textContent = "Key D was pressed!";
+            document.body.appendChild(newParagraph);
+            break;
+        case "w":
+            h1.style.display = h1.style.display === "none" ? "block" : "none";
+            break;
+        case " ":
+            h1.style.fontSize = (parseInt(h1.style.fontSize) || 16) + 5 + "px";
+            break;
+    }
+});
