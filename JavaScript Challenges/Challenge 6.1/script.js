3f8ac2649620d1827a79d2c5cd797bb60c147772
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-08-29 09:58:15 +0300

    Challenge 6.1 Done with step3

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -1,10 +1,26 @@
 function delay(milliseconds) {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve();
+        resolve("Done");
       }, milliseconds);
     });
   }
   console.log(delay(2000));
 
+  fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+  .then(response =>response.json())
+  .then(data =>{
+    console.log(data.text);
+  });
+
+  async function fetchRandomFactAsync() {
+    try {
+      const response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
+      const data = await response.json();
+      console.log(data.text);
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  }
+  fetchRandomFactAsync();
   
